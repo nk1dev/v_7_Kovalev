7db11ce9471fffb48e49f6a7dab3b736391a3071
nk1dev <76609654+nk1dev@users.noreply.github.com>
2020-12-27 14:24:31 +0300

    f

---
+++ app/src/main/java/com/example/v_7_kovalev/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.v_7_kovalev;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+
+public class MainActivity extends AppCompatActivity {
+    float x1, x2,y1,y2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public boolean onTouchEvent(MotionEvent touchEvent){
+        switch(touchEvent.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                x1 = touchEvent.getX();
+                y1 = touchEvent.getY();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = touchEvent.getX();
+                y2 = touchEvent.getY();
+                if(x1>x2) {
+                    Intent i = new Intent(MainActivity.this, SwipeRight.class);
+                    startActivity(i);
+                }
+                break;
+
+        }
+        return false;
+    }
+}
